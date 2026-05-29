ac5689c5caf4f56aa8b8f6a051ce47bb2aa29dd2
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-29 12:31:27 +0100

    refactor: remove unused components and clean up Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,3 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Features from "../components/Features";
-import Programs from "../components/Programs";
-import Gallery from "../components/Gallery";
-import Testimonials from "../components/Testimonials";
-import CTA from "../components/CTA";
-import Footer from "../components/Footer";
-
 export default function Home() {
-  return (
-    <div className="font-sans text-zinc-900 antialiased">
-      <Navbar />
-      <main className="pt-24">
-        <Hero />
-        <About />
-        <Features />
-        <Programs />
-        <Gallery />
-        <Testimonials />
-        <CTA />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <div className="font-sans text-zinc-900 antialiased"></div>;
 }
